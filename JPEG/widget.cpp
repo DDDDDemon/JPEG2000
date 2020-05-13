5df03caa1ffa9b713b5a5d2ef079a7ef66682b57
DDDDDemon <1559956320@qq.com>
2020-05-13 20:42:41 +0800

    界面设计

--- JPEG/widget.cpp
+++ JPEG/widget.cpp
@@ -1,4 +1,4 @@
-#include "widget.h"
+﻿#include "widget.h"
 #include "ui_widget.h"
 
 Widget::Widget(QWidget *parent) :
@@ -6,6 +6,8 @@ Widget::Widget(QWidget *parent) :
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    setWindowFlags(windowFlags()&~Qt::WindowMaximizeButtonHint);// 禁止最大化按钮
+    setFixedSize(this->width(),this->height());// 禁止拖动窗口大小
 }
 
 Widget::~Widget()
